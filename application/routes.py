43cda70da9fc9019ed7a23773da31e46d3cdbc43
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-02 21:04:19 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -4,15 +4,15 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", index=True)
 
 @app.route("/character")
 def character():
     characterClass = [{"ClassId":"11", "Classes":"Fighter","Ability":"Sword"},
         {"ClassId":"22", "Classes":"Wizard","Ability":"Spell"}]
     print(characterClass)
-    return render_template("character.html", characterClass=characterClass)
+    return render_template("character.html", characterClass=characterClass, character=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", login=True)
