c82e3fd186dca055341cd1f6cc4338c97512f08a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-04 23:28:24 -0700

    Add files via upload

--- application/forms.py
+++ application/forms.py
@@ -1,3 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms .validators import DataRequired
+
+class LoginForm(FlaskForm):
+    charName    =   StringField("Character Name", validators = [DataRequired()])
+    charClass   =   StringField("Character Class", validators = [DataRequired()])
+    rememberMe  =   BooleanField("Remember Me")
+    submit      =   SubmitField("Login")
+
+class CharacterForm(FlaskForm):
+    char_name   =   StringField("Character Name", validators = [DataRequired()])
+    char_class  =   StringField("Character Class", validators = [DataRequired()])
+    atk         =   StringField("Attack", validators = [DataRequired()])
+    ascii       =   StringField("Armor Class", validators = [DataRequired()])
+    submit      =   SubmitField("Login")
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KY = os.environ.get('SECRET_KEY') or "lyndakey"
+
+    MONGODB_SETTINGS = { 'db' : 'pylyndachardb'}
