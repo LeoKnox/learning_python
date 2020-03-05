1df0f95ce9c12783b444425d26ca47b68f93596e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-05 12:12:23 -0700

    Add files via upload

--- application/forms.py
+++ application/forms.py
@@ -1,16 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms .validators import DataRequired
+from wtforms .validators import DataRequired, Length, EqualTo
+from application.models import Character
 
 class LoginForm(FlaskForm):
-    charName    =   StringField("Character Name", validators = [DataRequired()])
+    charName    =   StringField("Character Name", validators = [Length(min=3,max=25), DataRequired()])
     charClass   =   StringField("Character Class", validators = [DataRequired()])
     rememberMe  =   BooleanField("Remember Me")
     submit      =   SubmitField("Login")
 
 class CharacterForm(FlaskForm):
-    char_name   =   StringField("Character Name", validators = [DataRequired()])
+    char_name   =   StringField("Character Name", validators = [Length(min=3,max=25), DataRequired()])
     char_class  =   StringField("Character Class", validators = [DataRequired()])
     atk         =   StringField("Attack", validators = [DataRequired()])
-    ascii       =   StringField("Armor Class", validators = [DataRequired()])
+    ac          =   StringField("Armor Class", validators = [DataRequired()])
     submit      =   SubmitField("Login")
+
+    def validate_char_name(self,char_name):
+        character = Character.objects(char_name=char_name.data).first()
+        if character:
+            raise ValidationError("Name exists go forth and choose another.")
